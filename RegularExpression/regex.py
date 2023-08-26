4f53b69cfcbac41761104c94324c42d21d97fed2
vshopin <suvorovvv1@gamil.com>
2023-08-26 20:52:20 +0700

    parsed regular expressions in more detail

--- RegularExpression/regex.py
+++ RegularExpression/regex.py
@@ -1,6 +1,7 @@
 import re
 
 input_string = input()
-match = re.search(r'...\....', input_string)
+match = re.findall(
+    r'\d{2}\.\d{2}\.\d{4}|\d{2}\/\d{2}\/\d{4}|\d{4}\.\d{2}\.\d{2}|\d{4}\/\d{2}\/\d{2}', input_string)
 
-print(match[0])
+print(match)
